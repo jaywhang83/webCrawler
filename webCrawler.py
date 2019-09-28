526e50fac411ef09765ea85a7138acbceb381a2b
Jay Whang <jaywhang83@gmail.com>
2019-09-28 01:48:24 -0700

    Added depth counter

--- webCrawler.py
+++ webCrawler.py
@@ -14,34 +14,45 @@ def webCrawler(seedUrl, numPages):
 
     count = 0
     depth = 1
-    temp = {}
+    currentCount = 1
+    nextCount = 0
     while((not frontier.empty()) and (count < numPages and depth < 5)):
+        if count == currentCount:
+            depth += 1
+            currentCount = nextCount
+            nextCount = 0
+
         url = frontier.get()
-        print("Count : ", count, "opening: ", url)
+        # print("Depth : ", depth, "Count : ", count, "opening: ", url)
         # time.sleep(1)
+        
         req = urllib.request.Request(url)
         resp = urllib.request.urlopen(req)
         # print(resp.info())
-        # print(resp.geturl())
         respData = resp.read()
     
         paragraphs = re.findall(r'<p>(.*?)</p>',str(respData))
+
         count += 1
+
         links.append(url)
         pageSizes.append(len(respData))
 
-        temp[count] = url
         aTags = re.findall(r'href=[\'"/wiki/]?([^\'": >]+)',str(paragraphs))
+        # nextCount += len(aTags)
         # TODO
         # write to the file links and the size
         httpString = "https://en.wikipedia.org"
         wikiLinks = "/wiki/"
         # exclude = ":"
+        temp = 0
         for aTag in aTags:
             if aTag.startswith(wikiLinks):
                 fullUrl = httpString + aTag
                 frontier.put(fullUrl)
-            if len(aTags) == 0:
-                depth += 1
-
+                nextCount += 1
+                temp += 1
+       
+        print("count: ", count, "start url: ", url, "depth : ", depth, "number of links : " , temp)
+    print(depth)
 webCrawler(seedUrl, 1000)
