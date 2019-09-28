e20f6b4ab94eef26934d1b07b4311aa89b330ce1
Jay Whang <jaywhang83@gmail.com>
2019-09-28 02:55:14 -0700

    Added way to check if the webpage can be crawlered

--- webCrawler.py
+++ webCrawler.py
@@ -1,7 +1,9 @@
 import urllib.request
+import urllib.parse
 import re
 import queue
 import time
+import urllib.robotparser
 
 seedUrl = "https://en.wikipedia.org/wiki/Karen_Sp%C3%A4rck_Jones"
 
@@ -16,6 +18,7 @@ def webCrawler(seedUrl, numPages):
     depth = 1
     currentCount = 1
     nextCount = 0
+    base = "https://en.wikipedia.org"
     while((not frontier.empty()) and (count < numPages and depth < 5)):
         if count == currentCount:
             depth += 1
@@ -23,36 +26,40 @@ def webCrawler(seedUrl, numPages):
             nextCount = 0
 
         url = frontier.get()
-        # print("Depth : ", depth, "Count : ", count, "opening: ", url)
-        # time.sleep(1)
-        
-        req = urllib.request.Request(url)
-        resp = urllib.request.urlopen(req)
-        # print(resp.info())
-        respData = resp.read()
+        robotTxt = urllib.robotparser.RobotFileParser()
+        robotTxt.set_url(urllib.parse.urljoin(base, 'robots.txt'))
+        robotTxt.read()
+        print (url)
+        print(robotTxt.can_fetch("*", url))
+        if url not in links:
+            # print("Depth : ", depth, "Count : ", count, "opening: ", url)
+            time.sleep(1)
+            req = urllib.request.Request(url)
+            resp = urllib.request.urlopen(req)
+            # print(resp.info())
+            respData = resp.read()
     
-        paragraphs = re.findall(r'<p>(.*?)</p>',str(respData))
+            paragraphs = re.findall(r'<p>(.*?)</p>',str(respData))
 
-        count += 1
+            count += 1
 
-        links.append(url)
-        pageSizes.append(len(respData))
+            links.append(url)
+            # print(links)
+            pageSizes.append(len(respData))
 
-        aTags = re.findall(r'href=[\'"/wiki/]?([^\'": >]+)',str(paragraphs))
-        # nextCount += len(aTags)
-        # TODO
-        # write to the file links and the size
-        httpString = "https://en.wikipedia.org"
-        wikiLinks = "/wiki/"
-        # exclude = ":"
-        temp = 0
-        for aTag in aTags:
-            if aTag.startswith(wikiLinks):
-                fullUrl = httpString + aTag
-                frontier.put(fullUrl)
-                nextCount += 1
-                temp += 1
-       
+            aTags = re.findall(r'href=[\'"/wiki/]?([^\'": >]+)',str(paragraphs))
+            # nextCount += len(aTags)
+            # TODO
+            # write to the file links and the size
+            wikiLinks = "/wiki/"
+            # exclude = ":"
+            temp = 0
+            for aTag in aTags:
+                if aTag.startswith(wikiLinks):
+                    fullUrl = urllib.parse.urljoin(base, aTag)
+                    frontier.put(fullUrl)
+                    nextCount += 1
+                    temp += 1
         print("count: ", count, "start url: ", url, "depth : ", depth, "number of links : " , temp)
     print(depth)
 webCrawler(seedUrl, 1000)
